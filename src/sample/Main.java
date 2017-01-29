1085cd7a1d944ac36804fdb3e6e1bf6990f5e89b
fishyFrogFace <camillahallo@mail.com>
2017-01-29 10:31:29 +0100

    Add original images

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,14 +10,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/t1/4geometryDash.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 248, 370));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
+        System.out.println("\u2605");
     }
 }
